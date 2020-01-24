c93dff9443552da1f1799d36bd523126ad810da4
Alexander Simeonov <2390362s@student.gla.ac.uk>
2020-01-24 09:50:58 +0000

    Chapter 4.3 Serving Media

--- rango/views.py
+++ rango/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    returnString = """
-    Rango says here is the about page.
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <p> Rango says hey there partner!  </p>
-    <a href="/rango/about/">About</a>
-    </body>
-    </html>
-    """
-    return HttpResponse(returnString)
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request,template_name = "rango/index.html",context=context_dict)
+
 
 def about(request):
     returnString = """
